eb93073e142cc8c7f0ca13b598c3883906434ac5
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-04-15 09:58:19 +0200

    app initialize/ set up

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+const vars = {
+  primary_pale_blue: "hsl(225, 100%, 94%)",
+  primary_bright_blue: "hsl(245, 75%, 52%)",
+  neutral_pale_blue: "hsl(225, 100%, 98%))",
+  neutral_desaturated_blue: "hsl(224, 23%, 55%)",
+  dark_blue: "hsl(223, 47%, 23%)",
+
+  fs_paragraph: 16,
+  fw_regular: 500,
+  fw_middle: 700,
+  fw_bold: 600,
+
+  ff_base: "Red Hat Display",
+};
+
+function App() {
+  return <h1>Hello world!</h1>;
+}
+
+export default App;
